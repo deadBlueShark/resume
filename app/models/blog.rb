bf6371f87f1c68c44b15ed53ed03ae3f7a073f73
chiNguyenTrex <xspacechinguyen@gmail.com>
2018-03-17 17:04:56 +0700

    model validation

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,4 +3,7 @@ class Blog < ApplicationRecord
   friendly_id :title, use: :slugged
 
   enum status: {draft: 0, published: 1}
+
+  validates :title, presence: true
+  validates :body, presence: true
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+  validates :name, presence: true
+  validates :percent, presence: true
 end
