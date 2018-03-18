88fcd83b34ec82f5ac4d70d13c26126ff9bfb2bc
chiNguyenTrex <xspacechinguyen@gmail.com>
2018-03-18 14:00:37 +0700

    portfolio feature

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'faker'
 gem 'ffaker'
 gem 'friendly_id', '~> 5.1.0'
 # Use Redis adapter to run Action Cable in production
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,11 @@
 class Portfolio < ApplicationRecord
   extend FriendlyId
+
   friendly_id :title, use: :slugged
+
+  has_many :technologies
+
+  accepts_nested_attributes_for :technologies, reject_if: -> attr {attr[:name].blank?}
 
   validates :title, presence: true
   validates :subtitle, presence: true
