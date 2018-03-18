b0ebe778654b22af19813eeb60a3ff8eb0f4a8af
chiNguyenTrex <xspacechinguyen@gmail.com>
2018-03-18 23:41:16 +0700

    refractored: move set_source from application controller to concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Placeholder
   include DeviseParameterWhitelist
-
-  before_action :set_source
-
-  def set_source
-    session[:source] = params[:q] if params[:q].present?
-  end
+  include SetSource
 end
