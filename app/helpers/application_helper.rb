2ed9f1806a53c5aaf06e95deb8b0668214675418
chiNguyenTrex <xspacechinguyen@gmail.com>
2018-03-21 09:24:34 +0700

    interface for application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,12 @@ module ApplicationHelper
     title.blank? ? base_title : "#{title} | #{base_title}"
   end
 
-  def user_log_status
+  def user_log_status css_class
     if current_user.is_a? GuestUser
-      login_link = link_to "Login", new_user_session_path
-      signup_link = link_to "Register", new_user_registration_path
-      insert_content_to_tag(:div, login_link, "item") +
-      insert_content_to_tag(:div, signup_link, "item")
+      link_to("Login", new_user_session_path, class: css_class) +
+      link_to("Register", new_user_registration_path, class: css_class)
     else
-      logout_link = link_to "Logout", destroy_user_session_path, method: :delete
-      insert_content_to_tag :div, logout_link, "item"
+      link_to "Logout", destroy_user_session_path, method: :delete, class: css_class
     end
   end
 
@@ -40,11 +37,5 @@ module ApplicationHelper
     when :alert then "alert-warning"
     when :success then "alert-success"
     end
-  end
-
-  private
-
-  def insert_content_to_tag html_tag, content, html_class = nil
-    content_tag html_tag, content, class: html_class
   end
 end
